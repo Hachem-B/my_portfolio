3f73bba39a13f450e7ff332a13ecc3ba19842821
Hachem-B <bahlousshachem@gmail.com>
2021-07-22 20:34:35 +0100

    Updates

---
+++ precache-manifest.d856114faa1d62b7f422784b9a163555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce539f0f33106d97dc2c",
+    "url": "/my_portfolio/static/css/main.5e3e8719.chunk.css"
+  },
+  {
+    "revision": "ce539f0f33106d97dc2c",
+    "url": "/my_portfolio/static/js/main.ce539f0f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/my_portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "dcf3931afee046ce6d05",
+    "url": "/my_portfolio/static/js/runtime~main.dcf3931a.js"
+  },
+  {
+    "revision": "04cc49b30d252f37b6afc7cdbb5f1c40",
+    "url": "/my_portfolio/index.html"
+  }
+];
